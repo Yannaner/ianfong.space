0ea3aa0a5dc2a0a4fd271655cfc4b187cbb57ab9
Yannaner <100739121+Yannaner@users.noreply.github.com>
2025-01-29 09:20:01 -0500

    PharmaSee Project update

--- assets/js/hackathonData.js
+++ assets/js/hackathonData.js
@@ -1,17 +1,19 @@
 const hackathonProjects = [
   {
     id: 1,
-    title: "SustainAI",
-    event: "HackPrinceton 2024",
-    date: "November 2024",
-    thumbnail: "./assets/images/sustainai.png",
-    description: "SustainAI is an AI-powered web application designed to serve as your personal sustainability assistant. Through an interactive chatbot interface, users receive personalized recommendations on how to reduce their environmental impact. Whether it's suggesting energy-saving habits, recommending eco-friendly products, or providing sustainable transportation options, SustainAI tailors its advice to fit each user's unique lifestyle.",
-    technologies: ["Next.js", "React","Python","MATLAB","MongoDB","TailWindCSS", "OpenAI"],
-    achievement: "First Place at Amazon Sponsored Best use of GenAI",
-    demoLink: "https://dorahacks.io/buidl/19493/",
-    githubLink: "https://github.com/aymane-omari/HackPrinceton",
-    teamMembers: ["Ian Fong", "Aymane Omari", "Salman Elgamal","Bimarsha Adhikari"]
+    title: "PharmaSee",
+    event: "HoyaHacks 2025",
+    date: "Janurary 2024",
+    thumbnail: "./assets/images/PharmaSee.png",
+    description: "The inspiration behind PharmaSee stems from the alarming rate of medication prescription errors, up to 25% annually. These errors can lead to severe health complications and even fatalities. Our goal with PharmaSee is to bridge the communication gap between doctors, pharmacies, and patients by leveraging technology to ensure medication accuracy and adherence.",
+    technologies: ["Next.js", "React","MongoDB","Flask","OCR"],
+    achievement: "",
+    demoLink: "https://devpost.com/software/pharmasee-cyhgki",
+    githubLink: "https://github.com/Steven-UY/hoyahacks25",
+    teamMembers: ["Ian Fong", "Chris Jin", "Steven Uy","Omobolaji Alabi"]
   },
+  
+  
   {
     id: 1,
     title: "DataMesh",
@@ -24,6 +26,22 @@ const hackathonProjects = [
     demoLink: "https://www.youtube.com/watch?v=aw9_EpVe_Qo",
     githubLink: "https://github.com/Datamesh-Solana/DataMesh",
     teamMembers: ["Ian Fong", "DIVIJ", "Belenko","Sheng","Rojeng"]
+  },
+  
+  
+  
+  {
+    id: 1,
+    title: "SustainAI",
+    event: "HackPrinceton 2024",
+    date: "November 2024",
+    thumbnail: "./assets/images/sustainai.png",
+    description: "SustainAI is an AI-powered web application designed to serve as your personal sustainability assistant. Through an interactive chatbot interface, users receive personalized recommendations on how to reduce their environmental impact. Whether it's suggesting energy-saving habits, recommending eco-friendly products, or providing sustainable transportation options, SustainAI tailors its advice to fit each user's unique lifestyle.",
+    technologies: ["Next.js", "React","Python","MATLAB","MongoDB","TailWindCSS", "OpenAI"],
+    achievement: "First Place at Amazon Sponsored Best use of GenAI",
+    demoLink: "https://dorahacks.io/buidl/19493/",
+    githubLink: "https://github.com/aymane-omari/HackPrinceton",
+    teamMembers: ["Ian Fong", "Aymane Omari", "Salman Elgamal","Bimarsha Adhikari"]
   }
   
 ];
